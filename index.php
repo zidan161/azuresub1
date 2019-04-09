be883b0b59e083631e48138053cf120719742ee1
zidanaqilamuhammad <38450460+zidanaqilamuhammad@users.noreply.github.com>
2019-04-09 16:30:36 +0700

    Update index.php

--- index.php
+++ index.php
@@ -22,25 +22,20 @@
        Name  <input type="text" name="name" id="name"/></br></br>
        Email <input type="text" name="email" id="email"/></br></br>
        Job <input type="text" name="job" id="job"/></br></br>
-<input type="submit" name="submit" value="Submit"> 
-<style type="text/css">
- 	body { background-color: #EA3CA3;}
- </style>
-</input>
+       <input type="submit" name="submit" value="Submit" />
+       <input type="submit" name="load_data" value="Load Data" />
  </form>
  <?php
-    $host = "zidansqldatabase.database.windows.net";
-    $user = "zidansql";
-    $pass = "Zappsql12345";
-    $db = "zidanwebsql";
-
+    $host = "<Nama server database Anda>";
+    $user = "<Nama admin database Anda>";
+    $pass = "<Password admin database Anda>";
+    $db = "<Nama database Anda>";
     try {
         $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
         $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
     } catch(Exception $e) {
         echo "Failed: " . $e;
     }
-
     if (isset($_POST['submit'])) {
         try {
             $name = $_POST['name'];
@@ -56,7 +51,12 @@
             $stmt->bindValue(3, $job);
             $stmt->bindValue(4, $date);
             $stmt->execute();
-            try {
+        } catch(Exception $e) {
+            echo "Failed: " . $e;
+        }
+        echo "<h3>Your're registered!</h3>";
+    } else if (isset($_POST['load_data'])) {
+        try {
             $sql_select = "SELECT * FROM Registration";
             $stmt = $conn->query($sql_select);
             $registrants = $stmt->fetchAll(); 
@@ -80,8 +80,7 @@
         } catch(Exception $e) {
             echo "Failed: " . $e;
         }
-        echo "<h3>Your're registered!</h3>";
     }
  ?>
  </body>
- </html>
+</html>
