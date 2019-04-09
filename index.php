5b8d5b46146fd560cbeefc03ce2136ed1e4b1f05
zidanaqilamuhammad <38450460+zidanaqilamuhammad@users.noreply.github.com>
2019-04-09 16:34:38 +0700

    Update index.php

--- index.php
+++ index.php
@@ -26,10 +26,10 @@
        <input type="submit" name="load_data" value="Load Data" />
  </form>
  <?php
-    $host = "<Nama server database Anda>";
-    $user = "<Nama admin database Anda>";
-    $pass = "<Password admin database Anda>";
-    $db = "<Nama database Anda>";
+    $host = "https://zidanazurewebapp.azurewebsites.net";
+    $user = "zidansql";
+    $pass = "Zappsql12345";
+    $db = "zidanwebsql";
     try {
         $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
         $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
