433683837211738168459e6833591eaf4983329a
zidanaqilamuhammad <38450460+zidanaqilamuhammad@users.noreply.github.com>
2019-04-09 08:32:38 +0700

    Update index.php

--- index.php
+++ index.php
@@ -26,10 +26,10 @@
        <input type="submit" name="load_data" value="Load Data" />
  </form>
  <?php
-    $host = "<Nama server database Anda>";
-    $user = "<Nama admin database Anda>";
-    $pass = "<Password admin database Anda>";
-    $db = "<Nama database Anda>";
+    $host = "zappappdatabase.database.windows.net";
+    $user = "Zidan Aqila Muhammad";
+    $pass = "Zappsql12345";
+    $db = "newsql";
 
     try {
         $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
